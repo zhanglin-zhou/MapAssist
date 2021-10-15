e15738396ad600765534250b17f2d175bb35fd03
OneXDeveloper <onexdeveloper@example.com>
2021-10-15 16:22:58 -0600

    Move offset to Offsets file

--- Helpers/GameMemory.cs
+++ Helpers/GameMemory.cs
@@ -112,7 +112,7 @@ namespace D2RAssist.Helpers
                 WindowsExternal.ReadProcessMemory(processHandle, posYAddress, addressBuffer, addressBuffer.Length, out bytesRead);
                 UInt16 playerY = BitConverter.ToUInt16(addressBuffer, 0);
 
-                IntPtr uiSettingsPath = IntPtr.Add(processAddress, 0x2054322);
+                IntPtr uiSettingsPath = IntPtr.Add(processAddress, Offsets.InGameMap);
                 WindowsExternal.ReadProcessMemory(processHandle, uiSettingsPath, byteBuffer, byteBuffer.Length,
                     out bytesRead);
                 Boolean mapShown = BitConverter.ToBoolean(byteBuffer, 0);
@@ -125,7 +125,6 @@ namespace D2RAssist.Helpers
                     AreaId = (Area)dwLevelId,
                     Difficulty = aGameDifficulty,
                     MapShown = mapShown,
-                    GameWindowActive = WindowsExternal.GetActiveWindowTitle() == "Diablo II: Resurrected",
                 };
             }
             catch(Exception e)
