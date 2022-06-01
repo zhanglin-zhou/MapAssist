627431bdfd2278c1670da5162c7f825421f28c96
ItzRabbs <1294559+ItzRabbs@users.noreply.github.com>
2022-06-01 14:35:27 -0400

    Updated pattern for GetMenuDataOffset

--- Helpers/ProcessContext.cs
+++ Helpers/ProcessContext.cs
@@ -79,31 +79,13 @@ namespace MapAssist.Helpers
             return IntPtr.Add(_baseAddr, (int)(delta - 0x121 + offsetAddressToInt));
         }
 
-        public IntPtr GetMenuOpenOffset(byte[] buffer)
-        {
-            var pattern = new Pattern("8B 05 ? ? ? ? 89 44 24 20 74 07");
-            var patternAddress = FindPattern(buffer, pattern);
-
-            var offsetBuffer = new byte[4];
-            var resultRelativeAddress = IntPtr.Add(patternAddress, 2);
-            if (!WindowsExternal.ReadProcessMemory(_handle, resultRelativeAddress, offsetBuffer, sizeof(int), out _))
-            {
-                _log.Info($"Failed to find pattern {pattern}");
-                return IntPtr.Zero;
-            }
-
-            var offsetAddressToInt = BitConverter.ToInt32(offsetBuffer, 0);
-            var delta = patternAddress.ToInt64() - _baseAddr.ToInt64();
-            return IntPtr.Add(_baseAddr, (int)(delta + 6 + offsetAddressToInt));
-        }
-
         public IntPtr GetMenuDataOffset(byte[] buffer)
         {
-            var pattern = new Pattern("45 8B D7 4C 8D 05 ? ? ? ?");
+            var pattern = new Pattern("48 89 45 B7 4C 8D 35 ? ? ? ?");
             var patternAddress = FindPattern(buffer, pattern);
 
             var offsetBuffer = new byte[4];
-            var resultRelativeAddress = IntPtr.Add(patternAddress, 6);
+            var resultRelativeAddress = IntPtr.Add(patternAddress, 7);
             if (!WindowsExternal.ReadProcessMemory(_handle, resultRelativeAddress, offsetBuffer, sizeof(int), out _))
             {
                 _log.Info($"Failed to find pattern {pattern}");
@@ -112,7 +94,7 @@ namespace MapAssist.Helpers
 
             var offsetAddressToInt = BitConverter.ToInt32(offsetBuffer, 0);
             var delta = patternAddress.ToInt64() - _baseAddr.ToInt64();
-            return IntPtr.Add(_baseAddr, (int)(delta + 10 + offsetAddressToInt));
+            return IntPtr.Add(_baseAddr, (int)(delta + 11 + offsetAddressToInt));
         }
 
         public IntPtr GetMapSeedOffset(byte[] buffer)
